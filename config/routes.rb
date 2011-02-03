a00eba84775b611622992836db3755809b539896
Tim McCarty <mccarty.tim@gmail.com>
2011-02-03 01:40:07 -0500

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
+
   resources :users
-  
+  resources :sessions, :only => [:new, :create, :destroy]
   #legacy lines
   #get "pages/home"
   #get "pages/contact"
@@ -9,11 +10,15 @@ SampleApp::Application.routes.draw do
   
   #Time for the new stuff:
   
-  match '/signup', :to => 'users#new'
+  match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   
   match '/contact', :to => 'pages#contact'
-  match '/about', :to => 'pages#about'
-  match '/help', :to => 'pages#help'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
+  
   
   root :to => 'pages#home'
 
